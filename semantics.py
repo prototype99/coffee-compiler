9c90d1c89db683033de763f9933fdb8a82ecd21b
seirra blake <sophietheopossum@yandex.ru>
2021-09-28 12:00:08 +0100

    further complete code

--- semantics.py
+++ semantics.py
@@ -9,8 +9,27 @@ class CoffeeTreeVisitor(CoffeeVisitor):
     def __init__(self):
         self.stbl = SymbolTable()
 
+    def visitBlock(self, ctx):
+        if ctx.LCURLY() is not None:
+            self.stbl.pushFrame(method)
+            self.visitChildren(ctx)
+            self.stbl.popFrame()
+
+    def visitGlobal_decl(self, ctx):
+        line = ctx.start.line
+        var_type = ctx.var_decl().data_type().getText()
+        for i in range(len(ctx.var_decl().var_assign())):
+            var_id = ctx.var_decl().var_assign(i).var().ID().getText()
+            var_size = 8
+            var_array = False
+            var = Var(var_id, var_type, var_size, Var.GLOBAL, var_array, line)
+            self.stbl.pushVar(var)
+
     def visitProgram(self, ctx):
+        method = Method('main', 'int', ctx.start.line)
+        self.stbl.pushFrame(method)
         self.visitChildren(ctx)
+        self.stbl.popFrame()
 
 
 # load source code
