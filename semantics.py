85946e0804348317eb47580fa3fb322763dda709
seirra blake <sophietheopossum@yandex.ru>
2021-09-28 11:30:16 +0100

    improve PEP 8 conformance

--- semantics.py
+++ semantics.py
@@ -4,28 +4,30 @@ from CoffeeVisitor import CoffeeVisitor
 from CoffeeParser import CoffeeParser
 from CoffeeUtil import Var, Method, Import, Loop, SymbolTable
 
+
 class CoffeeTreeVisitor(CoffeeVisitor):
     def __init__(self):
         self.stbl = SymbolTable()
-        
+
     def visitProgram(self, ctx):
         self.visitChildren(ctx)
 
-#load source code
+
+# load source code
 filein = open('./test.coffee', 'r')
 source_code = filein.read();
 filein.close()
 
-#create a token stream from source code
+# create a token stream from source code
 lexer = CoffeeLexer(antlr.InputStream(source_code))
 stream = antlr.CommonTokenStream(lexer)
 
-#parse token stream
+# parse token stream
 parser = CoffeeParser(stream)
 tree = parser.program()
 
-#create Coffee Visitor object
+# create Coffee Visitor object
 visitor = CoffeeTreeVisitor()
 
-#visit nodes from tree root
+# visit nodes from tree root
 visitor.visit(tree)
