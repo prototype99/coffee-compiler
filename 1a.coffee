a03acff86f3cb334e22aa372c028ed5c6c2c55a6
seirra blake <sophietheopossum@yandex.ru>
2021-10-20 11:23:31 +0100

    add 1a test code

---
+++ 1a.coffee
@@ -0,0 +1,5 @@
+import printf, printf;
+void foo(int x, int y) {
+return 0;
+}
+int a = food(1, -2.0, 5);
--- visitor.py
+++ visitor.py
@@ -223,7 +223,8 @@ class CoffeeTreeVisitor(CoffeeVisitor):
 
 
 # load base test file
-filein = open('./test.coffee', 'r')
+#bfilein = open('./test.coffee', 'r')
+filein = open('./1a.coffee', 'r')
 # read whatever file was enabled
 source_code = filein.read()
 filein.close()
