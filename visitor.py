ecc1aff03787f1c5ac2759cf2543b7afa54eb394
seirra blake <sophietheopossum@yandex.ru>
2021-10-26 14:49:27 +0100

    be more specific about constant use

--- visitor.py
+++ visitor.py
@@ -13,7 +13,7 @@ from CoffeeParser import CoffeeParser
 # remember to add Loop for loops
 from CoffeeUtil import Var, Method, Import, SymbolTable
 
-# constant used to improve readability
+# constant used to improve codegen readability
 result = '%rax'
 
 
